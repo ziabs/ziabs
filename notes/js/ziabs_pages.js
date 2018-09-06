476418fe693eedce7bfdd03cc0c11a507025b995
Sharon Simon <sharon.simon@bookmyshow.com>
2018-09-06 16:26:39 +0530

    style fix

---
+++ notes/js/ziabs_pages.js
@@ -0,0 +1,5 @@
+;(function () {
+	
+    'use strict';
+    $( "#how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript" ).load( "pages/how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript.html" );
+}());    
