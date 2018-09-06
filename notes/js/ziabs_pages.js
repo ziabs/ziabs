e05250c1279b33f298def8852a064b39364f67da
Sharon Simon <sharon.simon@bookmyshow.com>
2018-09-06 19:45:29 +0530

    :|

--- notes/js/ziabs_pages.js
+++ notes/js/ziabs_pages.js
@@ -1,5 +1,26 @@
-;(function () {
-	
+;(function () {	
     'use strict';
+// REDIRECT
+var path = window.location.pathname;
+var pages=path.split("/pages/");
+if(pages.length>1){ 
+    var your_note=path.split('/');
+    (function() {
+        var script = document.createElement('script');
+        script.src = 'http://ziabs.com/notes/js/jquery.min.js';
+        script.type = 'text/javascript';
+        script.onload = function() {
+            var $ = window.jQuery;
+            $.each(your_note, function( index, value ) {
+            if(new RegExp('^\.*html$').test(value)){
+                var mypage=value.replace('%2f','');
+                window.location.href='http://www.ziabs.com/notes/#'+mypage;
+            }
+        });
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+    })();
+}
+//# YOUR PAGES GOES HERE
     $( "#how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript" ).load( "pages/how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript.html" );
 }());    
