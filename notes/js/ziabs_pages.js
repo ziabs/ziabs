34528027e2b469390c2b0919fc16cfd438405837
ziAbs <info.ziabs@gmail.com>
2018-09-07 23:38:31 +0530

    #tag

--- notes/js/ziabs_pages.js
+++ notes/js/ziabs_pages.js
@@ -1,7 +1,10 @@
 ;(function () {	
     'use strict'; 
 //Page Hash
-window.location.hash = window.location.hash;
+var hash=window.location.hash;
+window.location.hash = hash;
+$('meta[name=description]').attr('content', 'ziAbs notes'+hash );
+(hash)?$(document).prop('title','ziAbs-Notes '+hash):$(document).prop('title','ziAbs-Notes #'+$('.ziabs-title').eq(0)[0].id);
 //# YOUR PAGES GOES HERE
     $( "#how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript" ).load( "pages/how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript.html" );
     $( "#how-to-create-swap-on-centos7" ).load( "pages/how-to-create-swap-on-centos7.html" );
