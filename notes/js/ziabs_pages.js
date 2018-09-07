81847f1921a381076c14e40193feaeb43d5902b7
Sharon Simon <sharon.simon@bookmyshow.com>
2018-09-07 13:25:03 +0530

    how to create swap on centos 7

--- notes/js/ziabs_pages.js
+++ notes/js/ziabs_pages.js
@@ -23,4 +23,5 @@ if(pages.length>1){
 }
 //# YOUR PAGES GOES HERE
     $( "#how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript" ).load( "pages/how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript.html" );
+    $( "#how-to-create-swap-on-centos7" ).load( "pages/how-to-create-swap-on-centos7.html" );
 }());    
