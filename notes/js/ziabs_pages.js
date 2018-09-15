b585e41fdba300a7370f3eb03188b504ea3e1528
ziAbs <info.ziabs@gmail.com>
2018-09-15 11:56:10 +0530

    changing-user-password-using-terminal

--- notes/js/ziabs_pages.js
+++ notes/js/ziabs_pages.js
@@ -6,6 +6,7 @@ window.location.hash = hash;
 $('meta[name=description]').attr('content', 'ziAbs notes'+hash );
 (hash)?$(document).prop('title','ziAbs-Notes '+hash):$(document).prop('title','ziAbs-Notes #'+$('.ziabs-title').eq(0)[0].id);
 //# YOUR PAGES GOES HERE
-    $( "#how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript" ).load( "pages/how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript.html" );
+    $( "#changing-user-password-using-terminal" ).load( "pages/changing-user-password-using-terminal.html" );
     $( "#how-to-create-swap-on-centos7" ).load( "pages/how-to-create-swap-on-centos7.html" );
+    $( "#how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript" ).load( "pages/how-to-pass-a-variable-by-value-to-async-callbacks-in-javascript.html" );
 }());    
